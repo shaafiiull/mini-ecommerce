19530d0615b8eb60be25e4e9d8b7419a217b853a
shaafiiull <thisisshaafii@gmail.com>
2023-03-02 00:03:28 +0600

    now when the page first loads, the cart will be displayed based on saved data in local-storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -19,4 +19,15 @@ const addToDb = (id) => {
   localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 };
 
-export { addToDb };
+const getStoredCart = () => {
+  let shoppingCart = {};
+
+  //get the shopping cart from local storage
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  }
+  return shoppingCart;
+};
+
+export { addToDb, getStoredCart };
