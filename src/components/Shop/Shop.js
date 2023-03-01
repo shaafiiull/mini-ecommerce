e0763d63d79987553db2ff2c51d392051a678c6d
shaafiiull <thisisshaafii@gmail.com>
2023-03-01 23:18:55 +0600

    now add to cart btn is updating the cart component properly.now i have to save it to localstorage also

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,7 +13,21 @@ const Shop = () => {
   }, []);
   const handleAddToCart = (selectedProduct) => {
     console.log(selectedProduct); // checking if the add to cart btn is working
-    
+    let newCart = [];
+    const existedProduct = cart.find(
+      (product) => product.id === selectedProduct.id
+    );
+    if (!existedProduct) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const restProducts = cart.filter(
+        (product) => product.id !== selectedProduct.id
+      );
+      existedProduct.quantity = existedProduct.quantity + 1;
+      newCart = [...restProducts, existedProduct];
+    }
+    setCart(newCart);
   };
   return (
     <div className="shop-container">
